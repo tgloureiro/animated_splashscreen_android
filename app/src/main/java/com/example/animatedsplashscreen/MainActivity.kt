61d3e5e4a0ebd96179a0ca40668f5813495ec546
Tiago Loureiro <tgloureiro@gmail.com>
2021-07-05 22:55:33 -0300

    Splash with fade out animation using transitionDrawable

--- app/src/main/java/com/example/animatedsplashscreen/MainActivity.kt
+++ app/src/main/java/com/example/animatedsplashscreen/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.animatedsplashscreen
 
+import android.graphics.drawable.TransitionDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val splashFadeDurationMillis = 500
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val transitionDrawable = window.decorView.background as TransitionDrawable
+        transitionDrawable.startTransition(splashFadeDurationMillis)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        lifecycleScope.launch {
+            delay(splashFadeDurationMillis.toLong())
+            setContentView(R.layout.activity_main)
+        }
     }
 }
