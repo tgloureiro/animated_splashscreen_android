d38e74c40122463766f3836c05b0633383414c78
Tiago Loureiro <tgloureiro@gmail.com>
2021-07-10 16:28:18 -0300

    Add start screen animation

--- app/src/main/java/com/example/animatedsplashscreen/MainActivity.kt
+++ app/src/main/java/com/example/animatedsplashscreen/MainActivity.kt
@@ -11,32 +11,37 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
     companion object{
         const val logoCrossFadeDurationMillis = 300
-        // Flag that allows showing the splash screen only once
-        var splashDisplayed = false
+        const val spacingAfterFadeDurationMillis = 150
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if(!splashDisplayed){
+        // The savedInstanceState is not null on activity recreation
+        // (we must show splash screen only once)
+        val splashWasDisplayed = savedInstanceState != null
+        if(!splashWasDisplayed){
             // Splash is not shown, we should:
             // 1 - Start fading out the logo (cross fading the empty background)
             (window.decorView.background as TransitionDrawable).startTransition(
                 logoCrossFadeDurationMillis
             )
             // 2 = As we can't register a listener to be notified when the
-            // transition drawable finishes, launch a coroutine that blocks for
-            // the animation duration, flip the flag and set the content view after finishing
+            // transition drawable finishes, launches a coroutine that blocks for
+            // the animation duration, flips the flag and sets the content view after finishing
             lifecycleScope.launch {
-                delay(logoCrossFadeDurationMillis.toLong())
-                splashDisplayed = true
+                // Time between the cross fade and start screen animation
+                delay(logoCrossFadeDurationMillis.toLong() + spacingAfterFadeDurationMillis)
+                window.decorView.background = AppCompatResources.getDrawable(
+                    this@MainActivity,R.drawable.splash_background)
                 setContentView(R.layout.activity_main)
             }
         }else{
             // Splash was shown before, no need to animate the transition.
-            // 1 - Set the window background to the background without the logo (if needed)
+            // 1 - Sets the window background to the background without the logo (if needed)
             window.decorView.background = AppCompatResources.getDrawable(
                 this,R.drawable.splash_background)
-            // 2 - Set the content view instantly
+            // 2 - Sets the content view instantly
             setContentView(R.layout.activity_main)
         }
     }
+
 }
